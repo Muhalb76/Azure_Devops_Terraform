2a5db14444e99bfcf98e992f5eb594f8f255f957
Muhalb76 <56602823+Muhalb76@users.noreply.github.com>
2020-04-01 14:50:56 +0100

    Add files via upload

---
+++ winlogbeatinstall.ps1
@@ -0,0 +1,19 @@
+
+New-Item -Path 'D:\Winlogbeat' -ItemType Directory
+
+$client = New-Object system.net.WebClient
+$client.DownloadFile("https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-7.6.1-windows-x86_64.zip","d:\Winlogbeat\winlogbeat.zip")                           
+
+expand-Archive -Path 'D:\Winlogbeat\winlogbeat.zip' -DestinationPath 'C:\Program Files\'
+
+Rename-Item -Path "C:\Program Files\winlogbeat-7.6.1-windows-x86_64" -NewName "C:\Program Files\winlogbeat"
+
+
+. "C:\Program Files\winlogbeat\install-service-winlogbeat.ps1"
+
+$winlogsvc = Get-Service -Name winlogbeat     
+
+    if( $winlogsvc.Status -ne [System.ServiceProcess.ServiceControllerStatus]::Running)
+    {
+        $winlogsvc.Start()
+    }
